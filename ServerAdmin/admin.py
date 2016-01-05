980489d3c03d6bccaad8ab51612d73e73fc50ee1
Asayu123 <Asayu123@users.noreply.github.com>
2016-01-05 15:43:57 +0900

    1:Japanese support activated. 2:(temporally) superuser info noted into misc folder. 3:Added entry of application DB schema to django-admin dashboard.

--- ServerAdmin/admin.py
+++ ServerAdmin/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.DataCenter)
+admin.site.register(models.VCenterCluster)
+admin.site.register(models.RackPos)
+admin.site.register(models.HypervisorNodeTable)
+admin.site.register(models.HypervisorUsage)
+admin.site.register(models.HypervisorHost)
+admin.site.register(models.NFSStorage)
+admin.site.register(models.NFSVolume)
+admin.site.register(models.VirtualMachineBase)
+admin.site.register(models.VmAttachedVirtualHDD)
+admin.site.register(models.VmNetworkInfo)
+admin.site.register(models.VmInstalledSoftware)
+admin.site.register(models.VmServerSpecTest)
+admin.site.register(models.VmChefRecipe)
