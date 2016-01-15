ddf4765e8b4ba713b7b855bba94f8a73d590cc57
Asayu123 <Asayu123@users.noreply.github.com>
2016-01-15 17:24:27 +0900

    edit model and admin settings

--- ServerAdmin/models.py
+++ ServerAdmin/models.py
@@ -14,7 +14,7 @@ class Environment(models.Model):
 class DataCenter(models.Model):
     #POS_CHOICES = (('front', 'フロント'), ('back', 'バック'), ('kibi', '機微'))
     name = models.CharField(max_length=32, primary_key=True)
-    environment_name = models.ForeignKey('ServerAdmin.Environment', to_field='name', on_delete=models.PROTECT)
+    environment_name = models.ForeignKey('Environment', to_field='name', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
@@ -32,7 +32,7 @@ class VCenterCluster(models.Model):
 class Rack(models.Model):
     name = models.CharField(max_length=64, primary_key=True)  # strings
     console_name = models.CharField(max_length=64, unique=True)  # string, match to node number,console number
-    environment_name = models.ForeignKey('ServerAdmin.Environment', to_field='name', on_delete=models.PROTECT)
+    environment_name = models.ForeignKey('Environment', to_field='name', on_delete=models.PROTECT)
     num_of_unit = models.IntegerField(default=32)
 
     def __str__(self):
@@ -51,10 +51,10 @@ class PhysicalServer(models.Model):
     name = models.CharField(max_length=64, unique=True)
     product_name = models.CharField(max_length=64)
     usage = models.CharField(max_length=128)
-    environment_name = models.ForeignKey('ServerAdmin.Environment', to_field='name', on_delete=models.PROTECT)
+    environment_name = models.ForeignKey('Environment', to_field='name', on_delete=models.PROTECT)
 
     #following info is used when visualizing server layout
-    rack_name = models.ForeignKey('ServerAdmin.Rack', to_field='name',on_delete=models.PROTECT)
+    rack_name = models.ForeignKey('Rack', to_field='name',on_delete=models.PROTECT)
     rack_unit_start = models.PositiveIntegerField()
     rack_unit_end = models.PositiveIntegerField()
 
@@ -78,9 +78,9 @@ class HypervisorHost(models.Model):
     modified_date = models.DateTimeField()
 
     # reference
-    vCenterCluster_name = models.ForeignKey('ServerAdmin.VCenterCluster', to_field='name', on_delete=models.PROTECT)
-    physicalserver_name = models.OneToOneField('ServerAdmin.PhysicalServer', to_field='name', on_delete=models.PROTECT)
-    hypervisorusage_name = models.ForeignKey('ServerAdmin.HypervisorUsage', to_field='name', on_delete=models.PROTECT)
+    vCenterCluster_name = models.ForeignKey('VCenterCluster', to_field='name', on_delete=models.PROTECT)
+    physicalserver_name = models.OneToOneField('PhysicalServer', to_field='name', on_delete=models.PROTECT)
+    hypervisorusage_name = models.ForeignKey('HypervisorUsage', to_field='name', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
@@ -96,8 +96,8 @@ class Datastore(models.Model):
 
 
 class NFS_Storage(models.Model): # Use this table to define NFS Datastore's network Path.
-    datastore_name = models.ForeignKey('ServerAdmin.Datastore', to_field='name', primary_key=True, on_delete=models.PROTECT)
-    ipaddr = models.IPAddressField()
+    datastore_name = models.OneToOneField('Datastore', to_field='name', primary_key=True, on_delete=models.PROTECT)
+    ipaddr = models.GenericIPAddressField()
     path = models.CharField(max_length=128)
 
     def __str__(self):
@@ -107,13 +107,13 @@ class NFS_Storage(models.Model): # Use this table to define NFS Datastore's netw
 class VLAN(models.Model):# Define Relationship between VLANID , Usage , IPRange
     id = models.IntegerField(primary_key=True, validators=[validators.MinValueValidator(0),validators.MaxValueValidator(4096)])
     usage = models.CharField(max_length=256)
-    nwaddr = models.IPAddressField()
+    nwaddr = models.GenericIPAddressField()
     prefix = models.IntegerField(validators=[validators.MinValueValidator(1), validators.MaxValueValidator(32)])
 
 
 class VirtualMachine(models.Model):
     # id = models.AutoField(primary_key=True) is defined Automatically, implicitly
-    # HDD and NIC information is stored in another class (i.e:List / Table)
+    # HDD and NIC information is stored in another schema (i.e:List / Table)
     name = models.CharField(max_length=64, primary_key=True)
     usage = models.CharField(max_length=256)
     os = models.CharField(max_length=64)
@@ -124,10 +124,10 @@ class VirtualMachine(models.Model):
 
     # following attribute is linked to another table.
 
-    hypervisor_name = models.ForeignKey('ServerAdmin.HypervisorHost', to_field='name', on_delete=models.PROTECT)
-    vminstalledsoftware_name = models.ManyToManyField('ServerAdmin.VmInstalledSoftware')
-    vmserverspectest_name = models.ManyToManyField('ServerAdmin.VmServerSpecTest')
-    vmchefrecipe_name = models.ManyToManyField('ServerAdmin.VmChefRecipe')
+    hypervisor_name = models.ForeignKey('HypervisorHost', to_field='name', on_delete=models.PROTECT)
+    vminstalledsoftware_name = models.ManyToManyField('VmInstalledSoftware')
+    vmserverspectest_name = models.ManyToManyField('VmServerSpecTest')
+    vmchefrecipe_name = models.ManyToManyField('VmChefRecipe')
 
     #following attribute is for logging.
     creation_date = models.DateTimeField()
@@ -139,19 +139,19 @@ class VirtualMachine(models.Model):
 
 class VmAttachedVirtualHDD(models.Model):
     id = models.AutoField(primary_key=True)
-    virtualmachine_name = models.ForeignKey('ServerAdmin.VirtualMachine', to_field='name',on_delete=models.CASCADE)
+    virtualmachine_name = models.ForeignKey('VirtualMachine', to_field='name',on_delete=models.CASCADE)
     mount_point = models.CharField(max_length=256)
     capacity_GB = models.PositiveIntegerField()  # Unit is GigaByte will be Stored
-    datastore_name = models.ForeignKey('ServerAdmin.Datastore', to_field='name', on_delete=models.PROTECT)
+    datastore_name = models.ForeignKey('Datastore', to_field='name', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.id
 
 
-class VmAttachedNIC(models.Model):
+class VmAttachedVirtualNIC(models.Model):
     id = models.AutoField(primary_key=True)
-    virtualmachine_name = models.ForeignKey('ServerAdmin.VirtualMachine', to_field='name', on_delete=models.CASCADE)  # vmID will be "id" that has been created in class "VirtualMachine"
-    vLAN_id = models.ForeignKey('ServerAdmin.VLAN', to_field='id',on_delete=models.PROTECT)
+    virtualmachine_name = models.ForeignKey('VirtualMachine', to_field='name', on_delete=models.CASCADE)  # vmID will be "id" that has been created in schema "VirtualMachine"
+    vLAN_id = models.ForeignKey('VLAN', to_field='id',on_delete=models.PROTECT)
     IP_Address = models.GenericIPAddressField()
 
     def __str__(self):
